bcf74386bc5d6bdc968d24fff69b8b47b0f42022
Thomas Broadley <buriedunderbooks@hotmail.com>
2016-07-07 12:31:26 +0100

    tentatively working

---
+++ index.js
@@ -0,0 +1,25 @@
+var glob = require('glob');
+var babel = require('babel-core');
+var plugin = require('babel-plugin-lodash-fp').default;
+var fs = require('fs');
+
+glob(process.argv[2], function (err, files) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  files.forEach(function (file) {
+    babel.transformFile(
+      file,
+      { plugins: [plugin] },
+      function(err, result) {
+        if (err) {
+          console.log(err);
+        } else {
+          fs.writeFile(file, result.code);
+        }
+      }
+    );
+  });
+});
